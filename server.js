d1eaafa9caedb2c8cdb8d5ce71397dc6daab9279
AgnesMuita <wamwithamuita@gmail.com>
2022-10-28 13:11:34 +0300

    update entry file

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const port=8000;
-const requestListener= function(req,res){
-  res.writeHead(200)
-  res.end("Heey")
-}
-const server = http.createServer(requestListener)
-server.listen(port)
